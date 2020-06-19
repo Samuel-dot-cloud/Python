76013a5d95411410f1b8b130445805263f3ab535
Samuel-dot-cloud <successam336@gmail.com>
2020-06-19 16:37:42 +0300

    first commit

---
+++ for.py
@@ -0,0 +1,6 @@
+prices = [100, 200, 300]
+
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
---
+++ nested.py
@@ -0,0 +1,5 @@
+numbers = [2, 2, 2, 2, 10]
+shape = 'x'
+for number in numbers:
+    for f in shape:
+        print(shape * number)
